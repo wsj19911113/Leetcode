8da98f252288a321fc0a9f8a74303914dfe35901
Sijia Wang <swang120@hawk.iit.edu>
2018-10-15 22:35:20 -0500

    206. Reverse Linked List

---
+++ linked_list/206. Reverse Linked List.java
@@ -0,0 +1,28 @@
+/*
+    206. Reverse Linked List
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        
+        while(current != null){
+            ListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        return previous;
+    }
+}
