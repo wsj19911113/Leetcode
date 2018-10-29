600480119fdcd8ef0445ffdbcb92d79f4eb39482
Fernando Wang <fwang@nfsmith.com>
2018-10-29 10:16:11 -0500

    syntax fix

--- linked_list/876. Middle of the linked List.java
+++ linked_list/876. Middle of the linked List.java
@@ -5,19 +5,18 @@
 */
 
 public ListNode middleNode(ListNode head) {
-        if(head == null || head.next == null) return head;
-        
-        ListNode slow = head;
-        ListNode fast = head;
-        
-        while(fast.next != null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        if(fast.next == null){
-            return slow;
-        } else {
-            return slow.next;
-        }
+    if(head == null || head.next == null) return head;
+    
+    ListNode slow = head;
+    ListNode fast = head;
+    
+    while(fast.next != null && fast.next.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    if(fast.next == null){
+        return slow;
+    } else {
+        return slow.next;
     }
 }
