9f201ab66fc59a6b9e7b8ade360dc2ece1194342
Fernando Wang <wsj19911113@hotmail.com>
2018-10-29 22:06:56 -0500

    added 92. Reverse linked list II

---
+++ linked_list/92. Reverse linked list II.java
@@ -0,0 +1,35 @@
+/*
+    92. Reverse linked list II
+*/
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if(head == null || head.next == null) return head;
+	    ListNode dummy = new ListNode(0);
+	    dummy.next = head;
+	    ListNode start = dummy;
+	    int index = 1;
+	    ListNode current = dummy.next;
+	    while(index < m){
+	        start = current;
+	        current = current.next;
+	        index++;
+	    }
+	    ListNode previous = start.next;
+	    if(previous.next == null) {
+	        return head;
+	    }
+	    current = current.next;
+	    ListNode temp = current.next;
+	    while(index < n){
+	        temp = current.next;
+	        current.next = previous;
+	        previous = current;
+	        current = temp;
+	        index++;
+	    }
+	    start.next.next = current;
+	    start.next = previous;
+	    return dummy.next;
+    }
+}
