ad1b47590765911fc8814c7aaf2b9cbd2a9df733
Fernando Wang <wsj19911113@hotmail.com>
2018-11-12 21:20:54 -0600

    added 119. Pascal's Triangle II

---
+++ array/119. Pascal's Triangle II.java
@@ -0,0 +1,19 @@
+/*
+    119. Pascal's Triangle II
+*/
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(rowIndex < 0) return result;
+        
+        for(int i = 0; i <= rowIndex; i++){
+            result.add(1);
+            for(int j = i-1; j > 0; j--){
+                result.set(j, result.get(j) + result.get(j-1));
+            }
+        }
+        
+        return result;
+    }
+}
